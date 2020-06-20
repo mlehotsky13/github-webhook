22a4e73e52b9144400b1db7bb1905dd51d875ab8
mlehotsky13 <mlehotsky13@gmail.com>
2020-06-20 13:39:40 +0200

    Update

--- src/main/java/eu/miroslavlehotsky/githubwebhook/service/GithubWebhookServiceImpl.java
+++ src/main/java/eu/miroslavlehotsky/githubwebhook/service/GithubWebhookServiceImpl.java
@@ -1,18 +1,30 @@
 package eu.miroslavlehotsky.githubwebhook.service;
 
-import java.text.MessageFormat;
+import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import eu.miroslavlehotsky.githubwebhook.model.GithubPushEvent;
 
 @Service
 public class GithubWebhookServiceImpl implements GithubWebhookService {
+	
+	@Value("${event.push.script}")
+	private String pushEventScript;
+	
+	@Value("${event.push.targetBranch}")
+	private String targetBranch;
 
 	@Override
 	public void processPushEvent(GithubPushEvent pushEvent) {
-		// TODO
-		String msg = "Received github push event from pusher {0}";
-		System.out.println(MessageFormat.format(msg, pushEvent.getPusher().getEmail()));
+		try {
+			String branch = pushEvent.getRef().split("/")[pushEvent.getRef().split("/").length - 1];
+			if (targetBranch.equals(branch)) {
+				Runtime.getRuntime().exec(new String[] { pushEventScript });
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
